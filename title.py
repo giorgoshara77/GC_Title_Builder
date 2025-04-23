13f0c806f762778e59cf403637ead6444f5ea561
giorgoshara77 <giorgos.hara77@gmail.com>
2025-04-23 23:23:21 +0300

    Update title.py

--- title.py
+++ title.py
@@ -162,9 +162,9 @@ def transform_title(raw_title, tags):
             raw_title_lower = title.lower()
 
             if color:
-                stone = f"{stone_shape} {color} {matched_type}".strip()
+                stone = f"{stone_shape + ' ' if stone_shape else ''}{color} {matched_type}".strip()
             else:
-                stone = f"{stone_shape} {matched_type}".strip()
+                stone = f"{stone_shape + ' ' if stone_shape else ''}{matched_type}".strip()
 
     # === METAL INFO ===
     plating_keywords = {
